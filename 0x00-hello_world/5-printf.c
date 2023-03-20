ae3d39b1d3ec66e12f0c5dfe612f430b4c3ead28
Eniola Kwuelum <capitise90@gmail.com>
2023-03-20 10:21:42 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Program to print an exact text using printf function
+ * Return: 0 (Successful)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}

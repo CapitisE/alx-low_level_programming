f197e94a4bfee00010c748629f91805a501ca02f
Eniola Kwuelum <capitise90@gmail.com>
2023-03-21 06:19:45 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Program to print lowercase alphabet in reverse.
+ * Return: 0 (Successful)
+ *
+ */
+
+int main(void)
+{
+	char reverselower;
+
+	for (reverselower = 'a'; reverselower <= 'z'; ++reverselower)
+	{
+	putchar (reverselower);
+	}
+	putchar ('\n');
+	return (0);
+
+}

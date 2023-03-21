75fd16300235188f2c9dfa52c6d69a1dd4cab8bf
Eniola Kwuelum <capitise90@gmail.com>
2023-03-21 06:37:10 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Program to print all the numbers of base 16 in lowercase.
+ * Return: 0 (Successful)
+ *
+ */
+
+int main(void)
+{
+	int lower16;
+	char upper16;
+
+	for (lower16 = '0'; lower16 <= '9'; lower16++)
+	putchar(lower16);
+
+	for (upper16 = 'a'; upper16 <= 'e'; upper16++)
+	putchar(upper16);
+	putchar('\n');
+
+	return (0);
+
+}

a827268cecaf0ee8892eb24cb1dc8eaa5cea114c
Eniola Kwuelum <capitise90@gmail.com>
2023-03-21 06:07:30 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Program to print all single digit numbers of base 10
+ * Return: 0 (Successful)
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	putchar(i);
+	putchar('\n');
+	}
+	return (0);
+
+}

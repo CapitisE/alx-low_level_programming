4dabe75e78bc9552247a29c14035307230cde937
Eniola Kwuelum <capitise90@gmail.com>
2023-03-21 05:28:46 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,10 +11,10 @@
 int main(void)
 {
 	char lower, upper;
-	
+
 	for (lower = 'a'; lower <= 'z'; lower++)
-        putchar(lower);
-	
+	putchar(lower);
+
 	for (upper = 'A'; upper <= 'Z'; upper++)
 	putchar(upper);
 	putchar('\n');

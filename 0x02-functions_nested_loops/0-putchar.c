1680829c76752e558ca1f6d58e62a581780d7310
Eniola Kwuelum <capitise90@gmail.com>
2023-03-21 09:42:44 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Program to print _putchar
+ * Return: 0(Successful)
+ *
+ */
+
+int main(void)
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
+
+	retutn (0);
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Readme to evaluate understanding of functions and nested loops in c

d53b900b67d4d0f8ebf2522a891d072375a78dcd
Eniola Kwuelum <capitise90@gmail.com>
2023-03-22 00:47:28 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Program to print 10 times the alphabet in lowercase
+ * print_alphabet_x10 - Program to print 10 times the alphabet in lowercase
  * Return: 0 (Successful|)
  *
  */

a0e0b4201e83785838c72b49927357c1a95ce79e
Eniola Kwuelum <capitise90@gmail.com>
2023-03-22 01:15:39 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,6 +2,9 @@
 
 /**
  * int _islower(int c) - Program to check for lowercase character
+ *
+ * int c: Serves a protype
+ * 
  * Return: 1 (if int c is lowercase), 0 (in all other conditions)
  *
  */

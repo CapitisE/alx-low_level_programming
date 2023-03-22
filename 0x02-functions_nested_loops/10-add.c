8df30e47766f3d3a1006b3a1fa377eb3a4490a8a
Eniola Kwuelum <capitise90@gmail.com>
2023-03-22 06:42:09 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Program to add two integers
+ * @n1: First integer
+ * @n2: Second integer
+ * Return: result
+ *
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}

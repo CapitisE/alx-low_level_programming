4ada7f3a715995779dfb9165c1903b269276d7aa
Eniola Kwuelum <capitise90@gmail.com>
2023-04-14 03:30:11 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -12,13 +12,13 @@
  */
 int is_digit(char *s)
 {
-	int i = 0;
+	int a = 0;
 
-	while (s[i])
+	while (s[a])
 	{
-		if (s[i] < '0' || s[i] > '9')
+		if (s[a] < '0' || s[a] > '9')
 			return (0);
-		i++;
+		a++;
 	}
 	return (1);
 }
@@ -58,44 +58,44 @@ void errors(void)
  */
 int main(int argc, char *argv[])
 {
-	char *s1, *s2;
-	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+	char *a, *b;
+	int length1, length2, length, i, carry, digit1, digit2, *ans, x = 0;
 
-	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+	a = argv[1], b = argv[2];
+	if (argc != 3 || !is_digit(a) || !is_digit(b))
 		errors();
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	len = len1 + len2 + 1;
-	result = malloc(sizeof(int) * len);
-	if (!result)
+	length1 = _strlen(a);
+	length2 = _strlen(b);
+	length = length1 + length2 + 1;
+	ans = malloc(sizeof(int) * length);
+	if (!ans)
 		return (1);
-	for (i = 0; i <= len1 + len2; i++)
-		result[i] = 0;
-	for (len1 = len1 - 1; len1 >= 0; len1--)
+	for (i = 0; i <= length1 + length2; i++)
+		ans[i] = 0;
+	for (length1 = length1 - 1; length1 >= 0; length1--)
 	{
-		digit1 = s1[len1] - '0';
+		digit1 = a[length1] - '0';
 		carry = 0;
-		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		for (length2 = _strlen(b) - 1; length2 >= 0; length2--)
 		{
-			digit2 = s2[len2] - '0';
-			carry += result[len1 + len2 + 1] + (digit1 * digit2);
-			result[len1 + len2 + 1] = carry % 10;
+			digit2 = b[length2] - '0';
+			carry += ans[length1 + length2 + 1] + (digit1 * digit2);
+			ans[length1 + length2 + 1] = carry % 10;
 			carry /= 10;
 		}
 		if (carry > 0)
-			result[len1 + len2 + 1] += carry;
+			ans[length1 + length2 + 1] += carry;
 	}
-	for (i = 0; i < len - 1; i++)
+	for (i = 0; i < length - 1; i++)
 	{
-		if (result[i])
-			a = 1;
-		if (a)
-			_putchar(result[i] + '0');
+		if (ans[i])
+			x = 1;
+		if (x)
+			_putchar(ans[i] + '0');
 	}
-	if (!a)
+	if (!x)
 		_putchar('0');
 	_putchar('\n');
-	free(result);
+	free(ans);
 	return (0);
 }
